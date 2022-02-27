9732bd33d5bae3799283229d6134ac9647a953ca
CJepps <cjepps@hotmail.co.uk>
2022-02-27 21:17:19 +0000

    Removed secret key and set debug.

--- crux/settings.py
+++ crux/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-irmkjeg9h!zx5*)*%%ti5w6=d4=t*t%5^-k3v*aolg@atu87(0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cjepps-crux.herokuapp.com', 'localhost']
 
