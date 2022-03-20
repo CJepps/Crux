604ac4e43d0be9805cc84b436723081a71181e94
CJepps <cjepps@hotmail.co.uk>
2022-03-20 19:36:43 +0000

    Added 'add_comment' view to views.py in products app. Created url pathway for add_comment.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from django.http import HttpResponseRedirect
+from .models import Product, CommentForm, Comment 
 
 # Create your views here.
 
@@ -40,3 +41,25 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_comment(request, id):
+    """A view to handle review submissions."""
+    url = request.META.get('HTTP_REFERER')
+  
+    if request.method == 'POST':  
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            data = Comment()  
+            data.subject = form.cleaned_data['subject']
+            data.comment = form.cleaned_data['comment']
+            data.rate = form.cleaned_data['rating']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.product_id = id
+            current_user = request.user
+            data.user_id = current_user.id
+            data.save()  # save data to table
+            messages.success(request, "Your review has been sent successfully ")
+            return HttpResponseRedirect(url)
+
+    return HttpResponseRedirect(url)
