01f08750fab07afc250079f2abf7058cd5a39d96
CJepps <cjepps@hotmail.co.uk>
2022-03-21 14:33:52 +0000

    settup admin class for blog app in django database models.

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Blogs
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'date',
+        'para1',
+        'para2',
+        'para3',
+    )
+
+
+admin.site.register(Blogs, BlogAdmin)
