a73234eb60700ebe43d5d14816201f4e595cc191
CJepps <cjepps@hotmail.co.uk>
2022-03-21 14:54:50 +0000

    Created new view in blogs app to display all blogs from database.

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+from .models import Blogs
 
-# Create your views here.
+
+def blog(request):
+    """ A view to display all blogs """
+    # Get all the blog posts in the database and order
+    # by date
+    blogs = Blogs.objects.all().order_by('-date')
+
+    context = {
+        'blogs': blogs,
+    }
+
+    return render(request, 'blogs/all_blogs.html', context)
--- crux/settings.py
+++ crux/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blogs',
     'crispy_forms',
     'storages',
 ]
