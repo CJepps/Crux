1615f5fcd4707297465fbce90c1afbaa9e8dfc0d
CJepps <cjepps@hotmail.co.uk>
2022-03-22 18:57:07 +0000

    Added url for add product template to navigation header.

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('addcomment/<int:id>', views.add_comment, name='addcomment'),
-    path('add/<int:id>', views.add_product, name='addproduct'),
+    path('add/', views.add_product, name='add_product'),
 ]
