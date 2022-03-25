344210ca2944c6378c5661cdc1185dda0f1ac9ee
CJepps <cjepps@hotmail.co.uk>
2022-03-25 08:58:39 +0000

    Set debug to false for production.

--- crux/settings.py
+++ crux/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production! use 'DEVELOPMENT' in os.environ
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cjepps-crux.herokuapp.com', 'localhost']
 
